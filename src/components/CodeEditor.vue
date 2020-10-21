b3acb8eec9f26d149930f516c4837d892ddbb148
Mesut Koca <imesutkoca@gmail.com>
2020-10-21 22:16:46 +0300

    update dependency and prismjs usage

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,39 +1,48 @@
 <template>
   <div>
-    <prism-editor
-      class="my-editor"
-      v-model="code"
-      :highlight="highlighter"
-      line-numbers
-    ></prism-editor>
+    <prism-editor class="my-editor" v-model="editorCode" :highlight="highlighter" line-numbers></prism-editor>
   </div>
 </template>
 
 <script>
 // import Prism Editor
-import { PrismEditor } from "vue-prism-editor";
-import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+import { PrismEditor } from 'vue-prism-editor'; //
+import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
 
 // import highlighting library (you can use any library you want just return html string)
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+import prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
 
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref } from 'vue';
 export default defineComponent({
   components: {
     PrismEditor,
   },
   setup() {
-    onMounted(() => {
-      console.log("OKKK");
-    });
-    const code = ref('console.log("Hello World")');
+    const editorCode = ref('console.log("Hello World")');
     const highlighter = (code) => {
-      return highlight(code.value, languages.js); // languages.<insert language> to return html with markup
+      return prism.highlight(code, prism.languages.js);
     };
-    return { code, highlighter };
+    return { editorCode, highlighter };
   },
 });
 </script>
+
+<style>
+.my-editor {
+  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+  background: #2d2d2d;
+  color: #ccc;
+
+  /* you must provide font-family font-size line-height. Example: */
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
