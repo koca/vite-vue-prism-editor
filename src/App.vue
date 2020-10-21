3af6d1159e000c386f14c316d3ac6180d89f20ba
Mesut <imesutkoca@gmail.com>
2020-10-21 20:18:39 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <CodeEditor />
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from "vue";
+import CodeEditor from "./components/CodeEditor.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    CodeEditor,
+  },
+  setup() {
+    onMounted(() => {
+      console.log("hey");
+    });
+  },
+});
+</script>
---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <prism-editor
+      class="my-editor"
+      v-model="code"
+      :highlight="highlighter"
+      line-numbers
+    ></prism-editor>
+  </div>
+</template>
+
+<script>
+// import Prism Editor
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+
+import { defineComponent, onMounted, ref } from "vue";
+export default defineComponent({
+  components: {
+    PrismEditor,
+  },
+  setup() {
+    onMounted(() => {
+      console.log("OKKK");
+    });
+    const code = ref('console.log("Hello World")');
+    const highlighter = (code) => {
+      return highlight(code.value, languages.js); // languages.<insert language> to return html with markup
+    };
+    return { code, highlighter };
+  },
+});
+</script>
